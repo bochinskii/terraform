d16f464e68e3c104abab9cd6fbea0ec15d0ae924
Denis Bochinskii <denis.bochinskii@gmail.com>
2022-05-11 10:03:38 +0300

    modify all

---
+++ 002_Create_LEMP_file/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_instance_my_lemp_public_ip" {
+  value = aws_instance.my_lemp.public_ip
+}
+
+output "aws_instance_my_lemp_public_dns" {
+  value = aws_instance.my_lemp.public_dns
+}
+
+output "aws_instance_my_lemp_arn" {
+  value = aws_instance.my_lemp.arn
+}
