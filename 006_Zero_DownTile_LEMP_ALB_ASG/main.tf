1629b9c87df1a3458afd1e18955e6d778483ea5a
Denis Bochinskii <denis.bochinskii@gmail.com>
2022-05-11 16:08:20 +0300

    Add 006

---
+++ 006_Zero_DownTile_LEMP_ALB_ASG/main.tf
@@ -0,0 +1,272 @@
+#
+# Lunch Template
+#
+resource "aws_launch_template" "my_lemp_template" {
+  name                   = "my_lemp_template"
+  image_id               = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.my_lemp_web.id,
+    aws_security_group.my_lemp_ssh.id
+  ]
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = var.volume_size
+      volume_type = var.volume_type
+      delete_on_termination = true
+    }
+  }
+
+  user_data = base64encode(templatefile("./user_data_http.sh.tftpl",
+  {
+    hostname = var.hostname,
+    timezone = var.timezone,
+    ssh_port = var.ssh_port,
+    mysql_repo = var.mysql_repo,
+    mysql_root_pass = var.mysql_root_pass,
+    mysql_admin_user = var.mysql_admin_user,
+    mysql_admin_user_pass = var.mysql_admin_user_pass,
+    mysql_drupal_user = var.mysql_drupal_user,
+    mysql_drupal_user_pass = var.mysql_drupal_user_pass,
+    mysql_drupal_db = var.mysql_drupal_db,
+    pkgs = var.pkgs,
+    ssl_cert = var.ssl_cert,
+    ssl_key = var.ssl_key,
+    site_dir = var.site_dir,
+    site_config = var.site_config
+  }
+  ))
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      var.template_tags,
+      {
+        Name = "my_lemp_${var.template_tags["Env"]}"
+      }
+    )
+  }
+
+}
+
+#
+# ALB
+#
+resource "aws_lb" "my_lemp_alb" {
+  name               = "my-lemp-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my_lemp_alb_sg.id]
+  subnets            = var.all_subnet_id
+
+  tags = merge(
+    var.template_tags,
+    {
+      Name = "my_lemp_alb_${var.template_tags["Env"]}"
+    }
+  )
+}
+
+# Listeners
+resource "aws_lb_listener" "my_lemp_alb_listener_80" {
+  load_balancer_arn = aws_lb.my_lemp_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    # type = "forward"
+    # target_group_arn = aws_lb_target_group.my_lemp_alb_tg.arn
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+
+  }
+}
+
+resource "aws_lb_listener" "my_lemp_alb_listener_443" {
+  load_balancer_arn = aws_lb.my_lemp_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-central-1:880954070217:certificate/b332d9f6-26ca-4635-9502-1d9e1e1ba4fb"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_lemp_alb_tg.arn
+  }
+}
+
+#
+# TG
+#
+resource "aws_lb_target_group" "my_lemp_alb_tg" {
+  name        = "my-lemp-alb-tg"
+  target_type = "instance"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    enabled = true
+    healthy_threshold = var.health_check["healthy_threshold"]
+    interval = var.health_check["interval"]
+    protocol = var.health_check["protocol"]
+    timeout = var.health_check["timeout"]
+    unhealthy_threshold = var.health_check["unhealthy_threshold"]
+    port = var.health_check["port"]
+  }
+
+  tags = merge(
+    var.template_tags,
+    {
+      Name = "my_lemp_alb_tg_${var.template_tags["Env"]}"
+    }
+  )
+}
+
+#
+# ASG
+#
+resource "aws_autoscaling_group" "my_lemp_asg" {
+  name               = "my_lemp_asg"
+  availability_zones = var.availability_zones
+  desired_capacity   = 1
+  max_size           = 2
+  min_size           = 1
+
+  health_check_type  = "ELB"
+
+  #target_group_arns = aws_lb_target_group.my_lemp_alb_tg.arn
+
+  launch_template {
+    id      = aws_launch_template.my_lemp_template.id
+    version = aws_launch_template.my_lemp_template.latest_version
+    #version = "$Latest"
+  }
+
+}
+
+resource "aws_autoscaling_attachment" "my_lemp_asg_attach" {
+  autoscaling_group_name = aws_autoscaling_group.my_lemp_asg.id
+  lb_target_group_arn    = aws_lb_target_group.my_lemp_alb_tg.arn
+}
+
+#
+# Security Groups
+#
+resource "aws_security_group" "my_lemp_alb_sg" {
+  name        = "my_lemp_alb_sg"
+  description = "Allow Web traffic"
+  vpc_id      = var.vpc_id
+
+
+  ingress {
+    description      = "To HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "To HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.template_tags,
+    {
+      Name = "my_lemp_alb_sg"
+    }
+  )
+}
+
+resource "aws_security_group" "my_lemp_web" {
+  name        = "my_lemp_web"
+  description = "Allow Web traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "All from ALB"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.my_lemp_alb_sg.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.template_tags,
+    {
+      Name = "my_lemp_web"
+    }
+  )
+}
+
+resource "aws_security_group" "my_lemp_ssh" {
+  name        = "my_lemp_ssh"
+  description = "Allow ssh traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "To SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "To custome SSH"
+    from_port        = var.ssh_port
+    to_port          = var.ssh_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.template_tags,
+    {
+      Name = "my_lemp_ssh"
+    }
+  )
+}
---
+++ 006_Zero_DownTile_LEMP_ALB_ASG/variables.tf
@@ -0,0 +1,120 @@
+#
+# Userd data variables
+#
+variable "hostname" {
+  type = string
+  default = "rocinante"
+}
+variable "timezone" {
+  type = string
+  default = "Europe/Berlin"
+}
+variable "ssh_port" {} # for user data and aws_security_group
+variable "mysql_repo" {
+  type = string
+  default = "https://dev.mysql.com/get/mysql80-community-release-el7-6.noarch.rpm"
+}
+variable "mysql_root_pass" {}
+variable "mysql_admin_user" {}
+variable "mysql_admin_user_pass" {}
+variable "mysql_drupal_user" {}
+variable "mysql_drupal_user_pass" {}
+variable "mysql_drupal_db" {}
+variable "pkgs" {
+  type = list(string)
+  default = [
+    "php", "php-fpm", "php-pdo", "php-mysqlnd", "php-xml", "php-gd", "php-curl",
+    "php-mbstring", "php-json", "php-common", "php-gmp", "php-intl", "php-gd", "php-cli", "php-zip", "php-opcache"
+  ]
+}
+variable "ssl_cert" {
+  type = string
+  default = "rocinante.crt"
+}
+variable "ssl_key" {
+  type = string
+  default = "rocinante.key"
+}
+variable "site_dir" {}
+variable "site_config" {
+  type = string
+  default = "rocinante.conf"
+}
+
+# aws_instance variables
+
+variable "ami" {
+  type = string
+  #default = "ami-05f5f4f906feab6a7"
+  default = "ami-09439f09c55136ecf"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type = string
+  default = "bochinskii_Frankfurt_2"
+}
+
+variable "availability_zone" {
+  type = string
+  default = "eu-central-1a"
+}
+
+####### root_block_device variables
+
+variable "volume_type" {
+  type = string
+  default = "gp2"
+}
+
+variable "volume_size" {
+  type = number
+  default = 10
+}
+
+# aws_security_group variables
+
+variable "vpc_id" {
+  type = string
+  default = "vpc-03ccdbfd7272a7584"
+}
+
+# ASG variables
+variable "availability_zones" {
+  type = list(string)
+  default = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
+
+# ALB variables
+
+variable "health_check" {
+   type = map
+   default = {
+     healthy_threshold = "3"
+     interval = "10"
+     protocol = "HTTP"
+     timeout = "2"
+     unhealthy_threshold = "2"
+     port = "80"
+  }
+}
+
+variable "all_subnet_id" {
+  type = list(string)
+  default = ["subnet-000c2008b7496a3b7", "subnet-0646580d441af171c", "subnet-053fd4df99fbbb0f9"]
+}
+
+# Other variables
+
+variable "template_tags" {
+  type = map
+  default = {
+    Owner = "Denis Bochinskii"
+    Project = "rocinante"
+    Env = "dev"
+  }
+}
